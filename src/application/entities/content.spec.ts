4a2355e9f1de81ad4b324e84a12867bcffb9e897
Gisele <gisesonia@gmail.com>
2022-12-14 19:54:50 -0300

    Agrupando testes

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,17 +1,19 @@
 import { Content  } from "./content";
 
-test('it should be able to create a notification content', () => {
-    const content = new Content('Você recebeu uma solicitação de amizade');
+describe('Notification content', () => {
+    it('it should be able to create a notification content', () => {
+        const content = new Content('Você recebeu uma solicitação de amizade');
+    
+        expect(content).toBeTruthy();
+    });
+    
+    it('it should not be able to create a notification content with less than 5 characters', () => {
+    
+        expect(() => new Content('aaa')).toThrow();
+    });
+    
+    it('it should not be able to create a notification content with less than 240 characters', () => {
+          expect(() => new Content('a'.repeat(241))).toThrow();
+    });
+})
 
-    expect(content).toBeTruthy();
-});
-
-test('it should not be able to create a notification content with less than 5 characters', () => {
-
-    expect(() => new Content('aaa')).toThrow();
-});
-
-test('it should not be able to create a notification content with less than 240 characters', () => {
-
-    expect(() => new Content('a'.repeat(241))).toThrow();
-});
