38f0b69f6f9fe94cd13aa7008c861d56a54ce066
Gisele <gisesonia@gmail.com>
2022-12-16 17:14:41 -0300

    Testes com repository em memoria

---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,12 @@
+import { Notification } from "../../src/application/entities/notification";
+import { NotificationsRepository } from "../../src/application/repositories/notifications-repository";
+
+
+
+export class InMemoryNotificationsRepository implements NotificationsRepository {
+    public notifications: Notification[] = [];
+
+    async create(notification: Notification){//Não esquecer de ver se o import do Notification é do entities
+        this.notifications.push(notification);
+    }
+}
